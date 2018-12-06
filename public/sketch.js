daacaf349ccb4a24f33df1dba1b335e26ef34bad
Joel Rusi <joel.r.rusi@gmail.com>
2018-12-06 20:43:58 +0200

    update main

--- public/sketch.js
+++ public/sketch.js
@@ -3,7 +3,7 @@ var players = [];
 var rpgs = [];
 var particles = [];
 var users = 0;
-var scl = 10;   //default scale
+var scl = 1;   //default scale
 var vertexes;
 var terrain = [];
 var stage = 0;
